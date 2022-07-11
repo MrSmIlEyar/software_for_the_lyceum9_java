39b1b0accb466df61be8602a356f4d6476f90f91
Fenikonir <belovemil@gmail.com>
2022-07-11 16:38:43 +0300

    Emil

--- app/src/main/java/com/example/mylyceum/main2.java
+++ app/src/main/java/com/example/mylyceum/main2.java
@@ -1,6 +1,7 @@
 package com.example.mylyceum;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -22,7 +23,6 @@ public class main2 extends AppCompatActivity {
 
         binding = ActivityMain2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
--- app/src/main/java/com/example/mylyceum/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mylyceum/ui/home/HomeFragment.java
@@ -1,18 +1,29 @@
 package com.example.mylyceum.ui.home;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.icu.lang.UCharacter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.mylyceum.R;
 import com.example.mylyceum.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    private int chet;
+
 
     private FragmentHomeBinding binding;
 
@@ -20,16 +31,28 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-
+        chet = 0;
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        LinearLayout linearLayout = binding.linearlayout;
+//        Добавление карточек с новостями
+        for (int i = 0; i < 100; i++) {
+            TextView textView = new TextView(getContext());
+            textView.setText("Hello World");
+            linearLayout.addView(textView);
+        }
+
+
+
+
 
         return root;
-    }
 
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
 }
