15a6bb48fe4af4f9fb3271a83fe4d1cce8baf741
Fenikonir <belovemil@gmail.com>
2022-07-13 16:47:37 +0300

    Emil

--- app/src/main/java/com/example/mylyceum/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/mylyceum/ui/dashboard/DashboardFragment.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,8 +26,6 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
