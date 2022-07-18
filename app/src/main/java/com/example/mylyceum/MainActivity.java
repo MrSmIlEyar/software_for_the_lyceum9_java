2e9020105e975311353c136dd8106fdd29865bf1
MrSmIlEyar <94910911+MrSmIlEyar@users.noreply.github.com>
2022-07-18 10:32:13 +0300

    new entry screen 2

--- app/src/main/java/com/example/mylyceum/MainActivity.java
+++ app/src/main/java/com/example/mylyceum/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
