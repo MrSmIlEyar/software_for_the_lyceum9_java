4dd7381f22cb1481227a4ee2713cdd8d9b4d023d
MrSmIlEyar <94910911+MrSmIlEyar@users.noreply.github.com>
2022-07-19 21:35:20 +0300

    connect to firebase and redesign

--- app/src/main/java/com/example/mylyceum/MainActivity.java
+++ app/src/main/java/com/example/mylyceum/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mylyceum;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,6 +13,11 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -27,71 +33,91 @@ public class MainActivity extends AppCompatActivity {
     private int id2; //режим глазика, когда пароль не виден
     private DatabaseReference accounts; //инициализируем БД
     private String groupUser = "Users";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        entrance = findViewById(R.id.entrance);
-        signup = findViewById(R.id.signup);
-        password = findViewById(R.id.password);
-        vk = findViewById(R.id.vk_link);
-//        eye = findViewById(R.id.eye);
-        login = findViewById(R.id.login);
-        flag = 1;
-        accounts = FirebaseDatabase.getInstance().getReference(groupUser);
-        id = getResources().getIdentifier("@android:drawable/ic_hidden", null, null);
-        id2 = getResources().getIdentifier("@android:drawable/ic_open", null, null);
-        password.setInputType(1); //устанавливаем обычный ввод с клавиатуры
-        //обрабатываем глазик для поля ввода пароля
-//        eye.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (flag == 1) {
-//                    eye.setImageResource(getResources().getIdentifier("@android:drawable/ic_ihidden.xml", null, null));
-//                    password.setTransformationMethod(new PasswordTransformationMethod());
-//                    flag = 0;
-//                } else {
-//                    eye.setImageResource(getResources().getIdentifier("@android:drawable/ic_ihidden.xml", null, null));
-//                    password.setTransformationMethod(null);
-//                    flag = 1;
-//                }
-//                password.setSelection(password.length());
-//            }
-//        });
-        //обработка глазика для воода пароля окончена
 
-        //обработка кнопки входа
-        entrance.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View view) {
-                String userlogin = login.getText().toString();
-                String userpassword = login.getText().toString();
-                if (!TextUtils.isEmpty(userlogin) && !TextUtils.isEmpty(userpassword)) {
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+
+                } else {
+                    // User is signed out
+
+                }
+
+            }
+        };
+
+        login = (EditText) findViewById(R.id.login);
+        password = (EditText) findViewById(R.id.password);
+
+
+        findViewById(R.id.entrance).setOnClickListener(this::onClick);
+        findViewById(R.id.signup).setOnClickListener(this::onClick);
+        findViewById(R.id.vk_link).setOnClickListener(this::onClick);
+    }
+
+    public void onClick(View view) {
+        if(view.getId() == R.id.entrance)
+        {
+            signin(login.getText().toString(),password.getText().toString());
+        }
+        else if (view.getId() == R.id.signup)
+        {
+            registration(login.getText().toString(),password.getText().toString());
+//            Intent intent = new Intent(MainActivity.this, signup.class);
+//            startActivity(intent);
+        }
+        else if (view.getId() == R.id.vk_link)
+        {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/pushkinisti"));
+            startActivity(intent);
+        }
+
+    }
+
+
+    public void signin(String email , String password)
+    {
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Aвторизация успешна", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(MainActivity.this, main2.class);
                     startActivity(intent);
+
                 }
-                else {
-                    Toast.makeText(MainActivity.this, "Есть пустое поле", Toast.LENGTH_SHORT).show();
+                else if (password.length() < 6)
+                {
+                    Toast.makeText(MainActivity.this, "Длина пароля меньше 6 символов", Toast.LENGTH_SHORT).show();
                 }
+                else
+                    Toast.makeText(MainActivity.this, "Aвторизация провалена", Toast.LENGTH_SHORT).show();
 
             }
         });
-        //обработка кнопки входа окончена
-        //обработка кнопки регистрации
-        signup.setOnClickListener(new View.OnClickListener() {
+    }
+    public void registration (String email , String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, signup.class);
-                startActivity(intent);
-            }
-        });
-        //обработка кнопки регистрации окончена
-        vk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/pushkinisti"));
-                startActivity(intent);
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(MainActivity.this, "Регистрация успешна", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(MainActivity.this, "Регистрация провалена", Toast.LENGTH_SHORT).show();
             }
         });
     }
